cb8126658dae917e31fb99fe78ad0966b8049903
JuliyaPershina <jpershina79@gmail.com>
2025-05-29 12:58:53 +0300

    riples не знакають у програвшого

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+const body = document.querySelector('body');
 const container = document.querySelector('.container');
+const wrapper = document.querySelector('.wrapper');
+wrapper.classList.add('invizible');
+
+const scoreNumber = document.querySelector('.scoreNumber');
+
 const buttonCircles = document.querySelectorAll('.buttonCircle');
 const buttonRulesUp = document.querySelector('.button-rules-up');
 const buttonRulesDown = document.querySelector('.button-rules-down');
-const popover = document.querySelector('.pop-rules');
+const popover = document.getElementById('pop-rules');
 
-
-
-const body = document.querySelector('body');
 const playAgain = document.querySelector('.play-again');
 const textPlayAgain = document.querySelector('.text-play-again');
 const userImage = document.querySelector('.your-pick > img');
@@ -15,10 +18,8 @@ const youWinText = document.querySelector('.you-win');
 const computerWinsText = document.querySelector('.computer-wins');
 const yourPickImage = document.querySelector('.your-pick-image');
 const computerPickImage = document.querySelector('.computer-pick-image');
-const wrapper = document.querySelector('.wrapper');
-wrapper.classList.add('invizible');
 
-
+let scor = 0;
 
 const choiceToImageMap = {
   0: { image: '2.svg', alt: 'scissors' },
@@ -28,13 +29,6 @@ const choiceToImageMap = {
   4: { image: '4.svg', alt: 'spock' },
 };
 
-// const winningCombos = {
-//   0: { beats: '2', losesTo: '1' }, // Камінь перемагає ножиці, програє паперу
-//   1: { beats: '0', losesTo: '2' }, // Папір перемагає камінь, програє ножицям
-//   2: { beats: '1', losesTo: '0' }, // Ножиці перемагають папір, програють каменю
-//   3: { beats: '1', losesTo: '0' }, // Ножиці перемагають папір, програють каменю
-//   4: { beats: '1', losesTo: '0' }, // Ножиці перемагають папір, програють каменю
-// };
 const winningCombos = {
   0: { beats: ['1', '3'], losesTo: '1' }, // scissors
   1: { beats: ['2', '4'], losesTo: '2' }, // paper
@@ -73,17 +67,6 @@ buttonCircles.forEach((button, i) => {
     </div></div>
     `;
 
-    const chekWinner = () => {
-      if (user === computer) {
-        textPlayAgain.textContent = 'draw';
-      } else {
-        if (isUserWinner) {
-          textPlayAgain.textContent = 'YOU WIN';
-        } else {
-          textPlayAgain.textContent = 'YOU LOOSE';
-        }
-      }
-    };
     const isUserWinner =
       winningCombos[user].beats[0] == computer ||
       winningCombos[user].beats[1] == computer;
@@ -93,12 +76,25 @@ buttonCircles.forEach((button, i) => {
     console.log(isUserWinner);
     console.log(`переможець ${winner}`);
 
+    const chekWinner = () => {
+      if (user === computer) {
+        textPlayAgain.textContent = 'draw';
+      } else {
+        if (isUserWinner) {
+          textPlayAgain.textContent = 'YOU WIN';
+          scor += 1;
+          scoreNumber.textContent = scor;
+        } else {
+          textPlayAgain.textContent = 'YOU LOOSE';
+        }
+      }
+    };
+
     chekWinner();
   });
 });
 
 playAgain.addEventListener('click', (event) => {
-
   const yourPickImage = document.querySelector('.your-pick-image');
   const computerPickImage = document.querySelector('.computer-pick-image');
 
@@ -107,53 +103,27 @@ playAgain.addEventListener('click', (event) => {
 
   yourPickImage.outerHTML = `<div class="your-pick-image"></div>`;
   computerPickImage.outerHTML = `<div class="computer-pick-image"></div>`;
-
 });
 
 buttonRulesUp.addEventListener('click', (event) => {
-  event.stopPropagation()
-  body.classList.toggle('body-dark')  
-})
+  event.stopPropagation();
+  body.classList.toggle('body-dark');
+});
 buttonRulesDown.addEventListener('click', (event) => {
-  event.stopPropagation()
-  body.classList.remove('body-dark')
-})
+  event.stopPropagation();
+  body.classList.remove('body-dark');
+});
 popover.addEventListener('click', (event) => {
-  event.stopPropagation()
+  event.stopPropagation();
   body.classList.add('body-dark');
-})
+});
 
 body.addEventListener('click', (event) => {
   body.classList.remove('body-dark');
-})
+});
 
-
-// buttonChoise.classList.add('circle-choise');
-
-// userImage.src = `./images/${choiceToImageMap[user].image}`;
-// userImage.alt = `${choiceToImageMap[computer].alt}`;
-
-// computerImage.src = `./images/${choiceToImageMap[computer].image}`;
-// computerImage.alt = `${choiceToImageMap[computer].alt}`;
-
-// if (isUserWinner) {
-//   body.classList.add('you-win');
-//   youWinText.textContent = `${winner} переміг`;
-// } else {
-//   body.classList.add('computer-wins');
-//   computerWinsText.textContent = `${winner} переміг`;
-//   textPlayAgain.textContent = `YOU LOSE`;
-// }
-
-// console.log(winningCombos);
-
-// Scissors beats Paper
-// - Paper beats Rock
-// - Rock beats Lizard
-// - Lizard beats Spock
-// - Spock beats Scissors
-// - Scissors beats Lizard
-// - Paper beats Spock
-// - Rock beats Scissors
-// - Lizard beats Paper
-// - Spock beats Rock
+// const YourPickRiples = document.querySelectorAll('.your-pick .riple');
+// const ComputerPickRiples = document.querySelectorAll(
+//   '.computer-pick .riple'
+// );
+// console.log(ComputerPickRiples);
